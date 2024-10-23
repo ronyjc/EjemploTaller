ce7757f5c0adca18fd0052ac39ad0e4e2d0a4426
ronyjc <ronyjcifuentes@gmail.com>
2024-10-22 20:25:24 -0600

    Se agrego la clase Camiones a solicitud del cliente

--- obj/Release/net8.0/EjercicioClases2.AssemblyInfo.cs
+++ obj/Release/net8.0/EjercicioClases2.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("EjercicioClases2")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2bf8d28d578850e0ce6b0b43ad792d2893b30a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("EjercicioClases2")]
